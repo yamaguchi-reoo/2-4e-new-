823a3e16e8ac3e90822b0aeb53f71af7bd9c8f06
kobayasihinata <120348245+kobayasihinata@users.noreply.github.com>
2023-04-24 11:29:20 +0900

    main追加

---
+++ 2-4e/main.cpp
@@ -0,0 +1,67 @@
+#include"DxLib.h"
+#include"Define.h"
+#include"SceneManager.h"
+#include"FpsController.h"
+#include"Title.h"
+#include "PadInput.h"
+
+
+#define FRAMERATE 60.0 //フレームレート
+
+/***********************************************
+ * プログラムの開始
+ ***********************************************/
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	LPSTR lpCmdLine, int nCmdShow)
+{
+	SetMainWindowText("りんごおとし");
+
+	ChangeWindowMode(TRUE);		// ウィンドウモードで起動
+
+	SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32);	//画面サイズの設定
+
+	if (DxLib_Init() == -1) return -1;	// DXライブラリの初期化処理
+
+	SetDrawScreen(DX_SCREEN_BACK);	// 描画先画面を裏にする
+
+	SceneManager* sceneMng;
+
+	try
+	{
+		sceneMng = new SceneManager((AbstractScene*)new Title());
+
+	}
+	catch (const char* err)
+	{
+		FILE* fp = NULL;
+
+		DATEDATA data;
+
+		GetDateTime(&data);
+		//ファイルオープン
+		fopen_s(&fp, "ErrLog.txt", "a");
+		//エラーデータの書き込み
+		fprintf_s(fp, "%02d年 %02d月 %02d日 %02d時 %02d分 %02d秒 : %sがありません。\n", data.Year, data.Mon, data.Day, data.Hour, data.Min, data.Sec, err);
+
+		return 0;
+	}
+	FpsController FPSC(FRAMERATE, 800);
+
+	// ゲームループ
+	while ((ProcessMessage() == 0) && (sceneMng->Update() != nullptr)) {
+
+		ClearDrawScreen();		// 画面の初期化
+		PAD_INPUT::UpdateKey();
+		sceneMng->Draw();
+		FPSC.All();
+
+		//強制終了
+		if (PAD_INPUT::OnButton(XINPUT_BUTTON_BACK))
+		{
+			break;
+		}
+
+		ScreenFlip();			// 裏画面の内容を表画面に反映
+	}
+	return 0;
+}
