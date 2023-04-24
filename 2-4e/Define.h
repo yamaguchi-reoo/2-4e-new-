a68c3b477138bf68c4b1e97f4875bee751aadc46
kobayasihinata <120348245+kobayasihinata@users.noreply.github.com>
2023-04-24 11:37:03 +0900

    ヘッダーファイル追加

---
+++ 2-4e/Define.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define SCREEN_HEIGHT 720	//画面サイズ (縦)
+#define SCREEN_WIDTH 1280	//画面サイズ (横)
+#define SCORE_UI_SIZE 180	//スコア表示エリアサイズ (横)
+
+#define TRUE	1	//真
+#define FALSE	0	//偽
---
+++ 2-4e/Title.h
@@ -0,0 +1,37 @@
+#pragma once
+#include "AbstractScene.h"
+
+class Title :
+    public AbstractScene
+
+{
+private:
+    int Select;         //選択数
+    int TitleImage;     //背景画像
+    int OldKey;		//前の入力キー
+    int NowKey;		//今の入力キー
+    int KeyFlg;		//入力キー情報
+
+    int MenuFont; //メニュー用のフォント
+
+    int TitleBGM;       //BGM
+    int MenuSE;         //選択SE
+    int DecisionSE;     //決定音SE
+    int mori_img;
+    int cursor_img;
+
+public:
+    //コンストラクタ
+    Title();
+
+    //デストラクタ
+    ~Title();
+
+    //描画以外の更新を実行
+    AbstractScene* Update() override;
+
+    //描画に関することを実装
+    void Draw() const override;
+};
+
+
